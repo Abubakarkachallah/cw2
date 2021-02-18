0fbae9b11861e8989e744760865bb00be9dfdc6a
Abubakar <kachallah27@yahoo.com>
2021-02-18 11:28:19 +0100

    created public directory. moved static files

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //import modules
 const exp = require('express')
 const { ObjectID } = require('mongodb')
+var path = require("path");
 
 //const bodyParser = require('body-parser')
 
@@ -9,6 +10,7 @@ const app = exp()
 
 //config express js
 app.use(exp.json())
+app.use(exp.static('public'))
 const port = process.env.PORT || 3000
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Headers", "*");
@@ -23,17 +25,26 @@ let db
 MongoClient.connect('mongodb+srv://Abubakarkachallah:Abba2001@cluster0.mhdye.mongodb.net', (err, client)=>{
     db = client.db('webstore')
 })
-
-//display a message or root path to show that API is working
-app.get('/', (req, res, next) => {
-    res.send('Select a collection, e.g., collection/messages')
-})
-
 //get collection
 app.param('collectionName', (req, res, next, collectionName)=>{
     req.collection = db.collection(collectionName)
     return next()
 })
+
+//display a message or root path to show that API is working
+app.get('/', (req, res, next) => {
+    // res.send('Select a collection, e.g., collection/messages')
+    res.render('index.html')
+    next()
+})
+
+// // middleware to handle images
+// app.get('/collection/lessons/img', (req, res, next) => {
+//     var imagePath = path.resolve(__dirname, "img"); 
+//     app.use("/img", express.static(imagePath));
+// })
+
+
 
 //retrieve all the objects from collection
 app.get('/collection/:collectionName', (req, res, next)=>{
